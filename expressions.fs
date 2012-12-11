2ced9f8dde17db73c4a559a802b6757a6289da41
corcorans <corcorans13@gmail.com>
2012-12-10 22:35:20 -0600

    Changed match case for Const and added code for adjacent_filter to test with.

--- expressions.fs
+++ expressions.fs
@@ -1,40 +1,43 @@
-(*
+﻿(*
 
-   Name:        Stephen Corcoran
-   Class:       CS 3520, Fall 2012
-   Assignment:  MP7
+Name: Stephen Corcoran
+Class: CS 3520, Fall 2012
+Assignment: MP7
 
 *)
 
+type Expr =
+     Const of int
+   | Add of Expr * Expr
+   | Subt of Expr * Expr
+   | Mult of Expr * Expr
+   | Div of Expr * Expr
+   | If of Expr * Expr * Expr
+
 
 let rec value = function
-   | Cont(x) -> x
+     Const(x) -> x
    | Add(x, y) -> value(x) + value(y)
    | Subt(x, y) -> value(x) - value(y)
    | Mult(x, y) -> value(x) * value(y)
    | Div(x, y) -> value(x) / value(y)
-   | If(x, y, z) -> if value(x) == 0 then value(y)
-                  else value(z)
-
-type Expr =
-     Const of int
-   | Add   of Expr * Expr
-   | Subt  of Expr * Expr
-   | Mult  of Expr * Expr
-   | Div   of Expr * Expr
-   | If    of Expr * Expr * Expr
+   | If(x, y, z) -> if value(x) = 0 then value(z)
+                    else value(y)
 
 
 let rec code = function
-   | Const(x)   -> [PUSH(x)]
-   | Add(l,  r) -> code(l) @ code(r) @ [ADD]
+   | Const(x) -> [PUSH(x)]
+   | Add(l, r) -> code(l) @ code(r) @ [ADD]
    | Subt(l, r) -> code(l) @ code(r) @ [SUB]
    | Mult(l, r) -> code(l) @ code(r) @ [MUL]
-   | Div(l,  r) -> code(l) @ code(r) @ [DIV]
-   | If(test, truepart, falsepart) -> 
+   | Div(l, r) -> code(l) @ code(r) @ [DIV]
+   | If(test, truepart, falsepart) ->
        let tp = code(truepart)
        let fp = code(falsepart)
        in code(test) @ [JIZ(List.length(tp) + 1)]
           @ tp @ [JMP(List.length(fp))] @ fp
 
-let adjacent_filter = function
+
+let rec adjacent_filter p q items = function
+    [] -> []
+    | x::l -> if p x then x::adjacent_filter p q l items else adjacent_filter p q l items 
