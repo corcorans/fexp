0b425ed87788890dda3b63a58836a82e19e27969
corcorans <corcorans13@gmail.com>
2012-12-10 18:12:30 -0600

    Added function value which recursively determines values.

--- expressions.fs
+++ expressions.fs
@@ -6,6 +6,16 @@
 
 *)
 
+
+let rec value = function
+   | Cont(x) -> x
+   | Add(x, y) -> value(x) + value(y)
+   | Subt(x, y) -> value(x) - value(y)
+   | Mult(x, y) -> value(x) * value(y)
+   | Div(x, y) -> value(x) / value(y)
+   | If(x, y, z) -> if value(x) == 0 then value(y)
+                  else value(z)
+
 type Expr =
      Const of int
    | Add   of Expr * Expr
@@ -13,9 +23,6 @@ type Expr =
    | Mult  of Expr * Expr
    | Div   of Expr * Expr
    | If    of Expr * Expr * Expr
-
-
-
 
 
 let rec code = function
@@ -29,3 +36,5 @@ let rec code = function
        let fp = code(falsepart)
        in code(test) @ [JIZ(List.length(tp) + 1)]
           @ tp @ [JMP(List.length(fp))] @ fp
+
+let adjacent_filter = function
